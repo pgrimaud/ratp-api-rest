8721562210bf05e25ca2960fac55faa451cc4fb0
Pierre Grimaud <pierre.grimaud@bigyouth.fr>
2017-02-16 19:25:12 +0100

    remove some controller logic to services

--- src/ApiBundle/Controller/TrafficController.php
+++ src/ApiBundle/Controller/TrafficController.php
@@ -27,19 +27,6 @@ class TrafficController extends Controller
         //  http://apixha.ixxi.net/APIX?keyapp=FvChCBnSetVgTKk324rO&cmd=getTrafficSituation&category=all&networkType=all&withText=true&apixFormat=json&tmp=1487092706473
 
         $payload = $this->get('api.traffic')->get('all');
-        dump($payload);
-        exit;
-        exit;
-        /** @var CacheItem $cachedData */
-        $cachedData = $this->get('api.storage')->getCache($hash);
-
-        if ($cachedData->isHit()) {
-            $payload = unserialize($cachedData->get());
-        } else {
-            $traffic = $this->get('api.traffic')->getAllTraffic();
-            $this->get('api.storage')->setCache($cachedData, $traffic);
-            $payload = unserialize($cachedData->get());
-        }
 
         $view = View::create($payload);
         $view->setFormat('json');
