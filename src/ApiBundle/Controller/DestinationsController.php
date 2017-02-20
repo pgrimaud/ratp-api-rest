cf3b4eecb52adc991c93e0f81cc3020d5ce5ec5d
Pierre Grimaud <pierre.grimaud@bigyouth.fr>
2017-02-20 18:09:55 +0100

    destinations

---
+++ src/ApiBundle/Controller/DestinationsController.php
@@ -0,0 +1,55 @@
+<?php
+namespace ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class DestinationsController extends Controller
+{
+    /**
+     * @ApiDoc(
+     *    description="Get destinations of a specific line",
+     *    section="Destinations",
+     *    requirements={
+     *      {
+     *          "name"="type",
+     *          "dataType"="string",
+     *          "description"="Type of transport (rers, metros, bus, tramways, noctiliens)"
+     *      },
+     *     {
+     *          "name"="code",
+     *          "dataType"="string",
+     *          "description"="Code of transport line"
+     *      }
+     *   }
+     * )
+     *
+     * @Rest\View()
+     * @Rest\Get("/destinations/{type}/{code}")
+     *
+     * @param Request $request
+     * @param $type
+     * @param $code
+     *
+     * @return View
+     */
+    public function destinationsAction(Request $request, $type, $code)
+    {
+        $parameters = [
+            'type' => $type,
+            'code' => $code
+        ];
+
+        $payload = $this->get('api.destinations')->get('line', $parameters);
+
+        if (!$payload) {
+            return $this->get('api.response')->notFound();
+        }
+
+        return $this->get('api.response')->format($payload);
+    }
+
+}
---
+++ src/ApiBundle/Services/Api/DestinationsService.php
@@ -0,0 +1,66 @@
+<?php
+namespace ApiBundle\Services\Api;
+
+use ApiBundle\Helper\NetworkHelper;
+use Ratp\Api;
+use Ratp\Direction;
+use Ratp\Directions;
+use Ratp\Line;
+use Ratp\Reseau;
+
+class DestinationsService extends ApiService implements ApiDataInterface
+{
+    /**
+     * @param $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function get($method, $parameters = [])
+    {
+        return parent::getData($method, $parameters);
+    }
+
+    /**
+     * @param $parameters
+     * @return array|null
+     */
+    protected function getLine($parameters)
+    {
+        $destinations = [];
+
+        $typesAllowed = [
+            'rers',
+            'metros',
+            'tramways',
+            'bus',
+            'noctiliens'
+        ];
+
+        if (!in_array($parameters['type'], $typesAllowed) || empty($parameters['code'])) {
+            return null;
+        }
+
+        $networkRatp = NetworkHelper::typeSlug($parameters['type'], true);
+
+        $reseau = new Reseau();
+        $reseau->setCode($networkRatp);
+
+        $line = new Line();
+        $line->setReseau($reseau);
+        $line->setCode($parameters['code']);
+
+        $directionsApi = new Directions($line);
+
+        $api    = new Api();
+        $return = $api->getDirections($directionsApi)->getReturn();
+
+        foreach ($return->getDirections() as $direction) {
+            /** @var Direction $direction */
+            $destinations[] = [
+                'name' => $direction->getName(),
+                'way'  => $direction->getSens()
+            ];
+        }
+        return $destinations;
+    }
+}
