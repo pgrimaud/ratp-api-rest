bf2ed8a88457970604a26f2310ba72c57ed5d596
Pierre Grimaud <grimaud.pierre@gmail.com>
2021-02-16 10:04:09 +0100

    Update symfony insight

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class                        => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class                        => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class                                  => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class                      => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class                              => ['all' => true],
 ];
